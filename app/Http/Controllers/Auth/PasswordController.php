c90925e70252af0159d1577176d2caf0540421af
Kazuto Takashima <takashima@MacBookPro.local>
2024-05-29 06:56:13 +0900

    パスワードのバリデーションを設定

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -17,7 +17,14 @@ class PasswordController extends Controller
     {
         $validated = $request->validateWithBag('updatePassword', [
             'current_password' => ['required', 'current_password'],
-            'password' => ['required', Password::defaults(), 'confirmed'],
+            'password' => ['required', Password::defaults(), 'confirmed', 'alpha_num'],
+        ],
+        [
+            'current_password.required'         => '現在のパスワードを入力してください。',
+            'current_password.current_password' => '現在のパスワードが間違っています。',
+            'password.required'                 => '新しいパスワードを入力してください。',
+            'password.min'                      => 'パスワードは :min 文字以上で設定してください。',
+            'password.alpha_num'                => 'パスワードは英数字のみで設定してください。',
         ]);
 
         $request->user()->update([
